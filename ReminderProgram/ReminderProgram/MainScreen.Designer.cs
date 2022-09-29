5fdf458c4928c6bf01f2f15feac2f4423cd88a9d
L-tenThije-S <0328786@student.rocvantwente.nl>
2022-09-29 17:40:18 +0200

    Changes to addscreen

--- ReminderProgram/ReminderProgram/MainScreen.Designer.cs
+++ ReminderProgram/ReminderProgram/MainScreen.Designer.cs
@@ -28,30 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.dataDates = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataDates)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // treeView1
+            // dataDates
             // 
-            this.treeView1.Location = new System.Drawing.Point(12, 12);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(238, 426);
-            this.treeView1.TabIndex = 0;
+            this.dataDates.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataDates.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataDates.Location = new System.Drawing.Point(343, 3);
+            this.dataDates.Name = "dataDates";
+            this.dataDates.RowTemplate.Height = 25;
+            this.dataDates.Size = new System.Drawing.Size(334, 429);
+            this.dataDates.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.dataDates, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dateTimePicker1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(680, 435);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(3, 3);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(334, 23);
+            this.dateTimePicker1.TabIndex = 1;
             // 
             // MainScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(262, 450);
-            this.Controls.Add(this.treeView1);
+            this.ClientSize = new System.Drawing.Size(704, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "MainScreen";
             this.Text = "Reminder Program";
+            ((System.ComponentModel.ISupportInitialize)(this.dataDates)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private TreeView treeView1;
+        private DataGridView dataDates;
+        private TableLayoutPanel tableLayoutPanel1;
+        private DateTimePicker dateTimePicker1;
     }
 }
